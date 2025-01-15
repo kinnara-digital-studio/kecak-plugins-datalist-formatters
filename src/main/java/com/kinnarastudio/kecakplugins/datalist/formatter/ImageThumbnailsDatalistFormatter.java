db96784e5e70238880cbd21041bc8716d113e33e
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2025-01-15 11:33:23 +0700

    add form, field, and size

--- src/main/java/com/kinnarastudio/kecakplugins/datalist/formatter/ImageThumbnailsDatalistFormatter.java
+++ src/main/java/com/kinnarastudio/kecakplugins/datalist/formatter/ImageThumbnailsDatalistFormatter.java
@@ -1,9 +1,7 @@
 package com.kinnarastudio.kecakplugins.datalist.formatter;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.joget.apps.app.model.AppDefinition;
@@ -14,6 +12,7 @@ import org.joget.apps.datalist.model.DataListColumn;
 import org.joget.apps.datalist.model.DataListColumnFormatDefault;
 import org.joget.apps.form.model.Form;
 import org.joget.plugin.base.PluginManager;
+import org.springframework.context.ApplicationContext;
 
 public class ImageThumbnailsDatalistFormatter extends DataListColumnFormatDefault{
     public final static String LABEL = "Image Thumbnails Formatter";
@@ -21,15 +20,34 @@ public class ImageThumbnailsDatalistFormatter extends DataListColumnFormatDefaul
     @Override
     public String format(DataList dataList, DataListColumn column, Object row, Object value) {
         final AppDefinition appDefinition = AppUtil.getCurrentAppDefinition();
+        final ApplicationContext applicationContext = AppUtil.getApplicationContext();
+        final PluginManager pluginManager = (PluginManager) applicationContext.getBean("pluginManager");
+
+        final Map<String, Object> dataModel = new HashMap<>();
+
         final String formDefId = getFormDefId();
-        final String primaryKeyValue = ((Map<String, String>)row).get("id");
-        return Optional.of(value)
+        final String fieldId = getFieldId();
+
+        final String primaryKeyValue = String.valueOf(((Map<?, ?>) row).get("id"));
+
+        final String[] links = Optional.ofNullable(fieldId.isEmpty() ? value : ((Map<?, ?>) row).get(fieldId))
                 .map(String::valueOf)
                 .map(s -> s.split(";"))
                 .stream()
                 .flatMap(Arrays::stream)
-                .map(filename -> String.format("<img src='/client/app/%s/%d/form/download/%s/%s/%s' style='width:100px;height:100px;'/>", appDefinition.getAppId(), appDefinition.getVersion(), formDefId, primaryKeyValue, filename))
-                .collect(Collectors.joining(" "));
+                .filter(Predicate.not(String::isEmpty))
+                .map(filename -> String.format("/web/client/app/%s/%d/form/download/%s/%s/%s.", appDefinition.getAppId(), appDefinition.getVersion(), formDefId, primaryKeyValue, filename))
+                .toArray(String[]::new);
+
+        dataModel.put("links", links);
+
+        final int width = getWidth();
+        dataModel.put("width", width);
+
+        final int height = getHeight();
+        dataModel.put("height", height);
+
+        return pluginManager.getPluginFreeMarkerTemplate(dataModel, getClassName(), "/templates/ImageThumbnailsDataListFormetter.ftl", null);
     }
 
     @Override
@@ -68,5 +86,25 @@ public class ImageThumbnailsDatalistFormatter extends DataListColumnFormatDefaul
 
     protected String getFormDefId() {
         return getPropertyString("formDefId");
+    }
+
+    protected String getFieldId() {
+        return getPropertyString("fieldId");
+    }
+
+    protected int getWidth() {
+        try {
+            return Integer.parseInt(getPropertyString("width"));
+        } catch (NumberFormatException e) {
+            return 50;
+        }
+    }
+
+    protected int getHeight() {
+        try {
+            return Integer.parseInt(getPropertyString("height"));
+        } catch (NumberFormatException e) {
+            return 50;
+        }
     }
 }
---
+++ src/main/resources/templates/ImageThumbnailsDataListFormetter.ftl
@@ -0,0 +1,3 @@
+<#list links as item>
+    <a href='${item}'><img src='${item}' style='width:${width!50}px;height:${height!50}px;'/></a>
+</#list>
